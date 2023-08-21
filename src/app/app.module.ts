171110b09139df79fcc216db1e711eaf4cf88512
deshanvimukthi-1999 <vimudeshan123@gmail.com>
2023-08-21 12:33:08 +0530

    add crud component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
 
 import { AppComponent } from './app.component';
 import { LanguageSwitcherComponent } from './language-switcher/language-switcher.component';
+import { DataEntryComponent } from './data-entry/data-entry.component'; // Import DataEntryComponent
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -14,11 +16,13 @@ export function HttpLoaderFactory(http: HttpClient) {
 @NgModule({
   declarations: [
     AppComponent,
-    LanguageSwitcherComponent
+    LanguageSwitcherComponent,
+    DataEntryComponent // Declare DataEntryComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule, // Add FormsModule
     TranslateModule.forRoot({
       defaultLanguage: 'en',
       loader: {
---
+++ src/app/data-entry/data-entry.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-entry',
+  templateUrl: './data-entry.component.html',
+  styleUrls: ['./data-entry.component.css']
+})
+export class DataEntryComponent {
+  items: string[] = [];
+  newItem: string = '';
+  editIndex: number | null = null;
+
+  addItem() {
+    if (this.newItem.trim() !== '') {
+      this.items.push(this.newItem);
+      this.newItem = '';
+    }
+  }
+
+  editItem(index: number) {
+    this.editIndex = index;
+    this.newItem = this.items[index];
+  }
+
+  saveEditedItem() {
+    if (this.editIndex !== null) {
+      this.items[this.editIndex] = this.newItem;
+      this.newItem = '';
+      this.editIndex = null;
+    }
+  }
+
+  deleteItem(index: number) {
+    this.items.splice(index, 1);
+  }
+}
