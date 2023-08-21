efc6ee21711adc030bccf177b8d389d179910120
deshanvimukthi-1999 <vimudeshan123@gmail.com>
2023-08-21 13:35:18 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { FormsModule } from '@angular/forms'; // Import FormsModule
+import { FormsModule } from '@angular/forms'; 
 
 import { AppComponent } from './app.component';
 import { LanguageSwitcherComponent } from './language-switcher/language-switcher.component';
-import { DataEntryComponent } from './data-entry/data-entry.component'; // Import DataEntryComponent
+import { DataEntryComponent } from './data-entry/data-entry.component'; 
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -17,12 +17,12 @@ export function HttpLoaderFactory(http: HttpClient) {
   declarations: [
     AppComponent,
     LanguageSwitcherComponent,
-    DataEntryComponent // Declare DataEntryComponent
+    DataEntryComponent 
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule, // Add FormsModule
+    FormsModule, 
     TranslateModule.forRoot({
       defaultLanguage: 'en',
       loader: {
