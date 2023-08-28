8eaddf758f5f001b19031bedcd4a905f41f494a6
deshanvimukthi-1999 <vimudeshan123@gmail.com>
2023-08-28 11:36:56 +0530

    update the code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,12 @@
 import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { Directionality } from '@angular/cdk/bidi';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  isRtl: boolean = false; // Initialize isRtl property
-
-  constructor(
-    private translate: TranslateService,
-    private dir: Directionality
-  ) {
-    translate.setDefaultLang('en');
-    this.setLanguageDirection();
-  }
-
-  switchLanguage(lang: string) {
-    this.translate.use(lang);
-    this.setLanguageDirection();
-  }
-
-  setLanguageDirection() {
-    const currentLang = this.translate.currentLang;
-    this.isRtl = currentLang === 'ar'; 
-  }
+ 
 }
